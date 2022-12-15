1ab6f9601a50a5f83a0d13c52e0b804d28c5d6ab
Jardon-Sammy <jardonsammy2@gmail.com>
2022-12-15 09:26:46 -0800

    task 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,12 @@
 #include "main.h"
-/**
- * _isdigit - function that checks for a digit (0 through 9)
- * @c: input
- * Return: 1 if c is a digit, 0 otherwise
- */
-int _isdigit(int c)
-{
 
-if ((c >= 48) && (c <= 57))
-return (1);
-else
-return (0)
+/**
+ * mul - a function that multiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result of the 2 inputs
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
