9d5494ffdc537141036aca5a6e8759338ce3c5b7
Jardon-Sammy <jardonsammy2@gmail.com>
2022-12-15 10:41:17 -0800

    alx project

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		if ((i == 2) || (i == 4))
-			continue;
-								
+			continue;				
 		else
+
 			_putchar(i + '0');
 	}
 	_putchar('\n');
