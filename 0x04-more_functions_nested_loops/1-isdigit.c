2828215b91489ee6b6568f09c63ae58af1074326
Jardon-Sammy <jardonsammy2@gmail.com>
2022-12-15 03:41:37 -0800

    0x04,c more  functions. more nested

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiples two intergers
+ * @a: firts input
+ * @b: second input
+ * Return: multiplication result
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
