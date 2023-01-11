9c1e28d63eb7025b7ddfb8bffc894c0bd504743e
Jardon-Sammy <jardonsammy2@gmail.com>
2023-01-11 02:34:29 -0800

    am not giving up

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Write a program that prints all arguments it receives
- *        All arguments should be printed, including the first on
+ * main - Prints all arguments it receives.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * @argc: This is the argument count 
- * @argv: This is the argument vector
- *
- * Return: 0;
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int count;
+	int arg;
 
-	for (count = 0; count < argc; count++)
-	{
-		printf("%s\n", argv[count]);
-	}
+	for (arg = 0; arg < argc; arg++)
+		printf("%s\n", argv[arg]);
+
 	return (0);
 }
