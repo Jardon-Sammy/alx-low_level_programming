0c12abd28c83c7922a33e450e157d9554dbc4be7
Jardon-Sammy <jardonsammy2@gmail.com>
2023-01-11 03:42:35 -0800

    Without libraries what have we? We have no past and no future

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+	_puts("\"am not giving up, am in love with alx africa, i want to be best software engineering\"\n\t- JArdon sammy");
+	        return (0);
+}
