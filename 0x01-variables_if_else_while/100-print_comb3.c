29e8997342d159cd927a1efae5f9ca83b3542908
Jardon-Sammy <jardonsammy2@gmail.com>
2023-05-19 19:32:39 -0700

    fixing betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
+ * main - program that prints all possible different combinations of two digits
+ * Numbers must be separated by ,followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Return: 0
  */
 int main(void)
 {
-	int digit1, digit2;
+	int i = 48;
+	int j;
+	int cal = 44;
+	int sp = 32;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (; i <= 57; i++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		j = i + 1;
+		for (; j <= 57; j++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+			putchar (i);
+			putchar (j);
+			if (i != 56 || (i == 56 && j != 57))
+			{
+				putchar (cal);
+				putchar (sp);
+			}
 		}
 	}
-
-	putchar('\n');
-
+	putchar ('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Entry point
  *
- * Return: Always 0.
- */
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	int num;
+	int num = 0;
 
-	for (num = 0; num <= 9; num++)
+	while (num <= 9)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
+		putchar(num + 48);
 
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		++num;
+	}
 	putchar('\n');
+
 	return (0);
 }
